034768e5a44f0f82129ec224940d341463bb8c07
piechartssuck <olgratefuldead@gmail.com>
2021-08-19 16:05:42 -0400

    edp613

---
+++ static/slides/Week 1/xaringanExtractR.R
@@ -0,0 +1,8 @@
+# Set the working directory as source ----
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+library(knitr)
+
+purl("Slides-Week-1.Rmd")
+purl("Slides-Week-1R.Rmd")
+pagedown::chrome_print("Slides-Week-1.html")
