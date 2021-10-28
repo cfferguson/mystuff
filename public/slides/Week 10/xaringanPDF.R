f2fe3b71313851cb5f62586242f93574ddac83d4
piechartssuck <olgratefuldead@gmail.com>
2021-10-28 15:37:20 -0400

    edp613

--- public/slides/Week 10/xaringanPDF.R
+++ public/slides/Week 10/xaringanPDF.R
@@ -1,6 +1,15 @@
 # Set the working directory as source ----
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-pagedown::chrome_print("Slides-Week-10.Rmd")
-pagedown::chrome_print("Slides-Week-10R.Rmd")
+# library(xaringanBuilder)
+# build_pdf("Slides-Week-5.Rmd")
+# build_pdf("Slides-Week-5.html")
 
+# pagedown::chrome_print("Slides-Week-5.Rmd")
+# pagedown::chrome_print("Slides-Week-5.html")
+# pagedown::chrome_print("Slides-Week-5R.Rmd")
+
+remotes::install_github("rstudio/chromote")
+source("https://git.io/xaringan2pdf")
+xaringan_to_pdf("Slides-Week-10.html")
+xaringan_to_pdf("Slides-Week-10R.html")
